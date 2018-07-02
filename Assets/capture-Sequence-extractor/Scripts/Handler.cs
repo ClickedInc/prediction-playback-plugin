e5916afbfc26b7fa04addc80d7b34e1aaa8cbc3a
1238qwer <1238qwer@naver.com>
2018-07-02 11:33:47 +0900

    first

---
+++ Assets/capture-Sequence-extractor/Scripts/Handler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Handler : MonoBehaviour {
+
+    public bool IsTimeWarping;
+    public bool IsPredicting;
+
+    public string folder;
+
+    public int captureLength;
+
+    [SerializeField] int captureFrame;
+    [SerializeField] int captureHeight;
+    [SerializeField] int captureWidth;
+
+    private Content cameraHandelr;
+
+    void Awake () {
+
+        Time.captureFramerate = captureFrame;
+
+        cameraHandelr = FindObjectOfType<Content>();
+
+        PlayerSettings.defaultScreenHeight = captureHeight;
+        PlayerSettings.defaultScreenWidth = captureWidth;
+
+    }
+
+	void Update () {
+
+
+      
+    }
+    
+    public void Exporting()
+    {
+        string prefabfilename = "Assets/Clicked";
+
+        string exportPath = "Assets/clicked.unitypackage";
+
+        AssetDatabase.ExportPackage(prefabfilename, exportPath,
+        ExportPackageOptions.IncludeDependencies | ExportPackageOptions.Recurse);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# prediction-playback-plugin
+# capture-sequence-extractor
+
+1. Place main / prefabs / ContentsCamera where you want to see the scene, like the main camera.
+
+2. Place main / prefabs / wrapper anywhere you want.
+
+3. fill in the Content component of the ContentsCamera.
+
+  - Head : main/Head
+  - TimeWarpingCamera : main/Head/TimeWarpingCamera
+  - Anchor : main/Anchor
+
+4. Please set handler.
+
+  - IsTimeWarping : set whether or not to time warp.
+
+  - IsPredicting : set whether or not to predict.
+
+  - Folder : set the path of the captured file.
+
+  - Capture Length : set the maximum number of captures.
+
+  - Capture Height , Capture Width :  set the resolution of the captured file.
